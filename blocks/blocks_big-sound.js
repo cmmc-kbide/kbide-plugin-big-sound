f4919119e2a8dc02180b6b69fd1679afbf19ed0a
Prawared Bowonphattharawadi <prawared.bw@gmail.com>
2019-08-26 18:12:21 +0700

    add generator

--- blocks/blocks_big-sound.js
+++ blocks/blocks_big-sound.js
@@ -1,18 +1,26 @@
-Blockly.Blocks['big_sound_block'] = {
-  init: function() {
-    this.appendDummyInput()
-        .appendField("BIG SOUND");
-    this.appendValueInput("BIG_SOUND_ANALOG_PIN")
-        .setCheck("Number")
-        .appendField("ANALOG PIN");
-    this.appendValueInput("BIG_SOUND_DIGITAL_PIN")
-        .setCheck("Number")
-        .appendField("DIGITAL PIN");
-    this.setInputsInline(true);
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setColour(230);
- this.setTooltip("");
- this.setHelpUrl("");
-  }
-};
+Blockly.Blocks["big_sound_block"] = {
+    init: function() {
+      this.appendDummyInput().appendField("BIG SOUND");
+      this.appendDummyInput()
+        .appendField("ANALOG PIN")
+        .appendField(new Blockly.FieldTextInput("A0"), "BIG_SOUND_ANALOG_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("BIG_SOUND_ANALOG_VALUE"),
+          "BIG_SOUND_ANALOG_VALUE"
+        );
+      this.appendDummyInput()
+        .appendField("DIGITAL PIN")
+        .appendField(new Blockly.FieldTextInput("0"), "BIG_SOUND_DIGITAL_PIN")
+        .appendField("SET TO")
+        .appendField(
+          new Blockly.FieldVariable("BIG_SOUND_DIGITAL_VALUE"),
+          "BIG_SOUND_DIGITAL_VALUE"
+        );
+      this.setPreviousStatement(true, null);
+      this.setNextStatement(true, null);
+      this.setColour(230);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
--- blocks/generators_big-sound.js
+++ blocks/generators_big-sound.js
@@ -1,7 +1,33 @@
-Blockly.JavaScript['big_sound_block'] = function(block) {
-  var value_big_sound_analog_pin = Blockly.JavaScript.valueToCode(block, 'BIG_SOUND_ANALOG_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  var value_big_sound_digital_pin = Blockly.JavaScript.valueToCode(block, 'BIG_SOUND_DIGITAL_PIN', Blockly.JavaScript.ORDER_ATOMIC);
-  // TODO: Assemble JavaScript into code variable.
-  var code = '...;\n';
-  return code;
-};
+Blockly.JavaScript["big_sound_block"] = function(block) {
+    var text_big_sound_analog_pin = block.getFieldValue("BIG_SOUND_ANALOG_PIN");
+    var variable_big_sound_analog_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("BIG_SOUND_ANALOG_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    var text_big_sound_digital_pin = block.getFieldValue(
+      "BIG_SOUND_DIGITAL_PIN"
+    );
+    var variable_big_sound_digital_value = Blockly.JavaScript.variableDB_.getName(
+      block.getFieldValue("BIG_SOUND_DIGITAL_VALUE"),
+      Blockly.Variables.NAME_TYPE
+    );
+    // TODO: Assemble JavaScript into code variable.
+    var code = `
+    #VARIABLE
+	int BIG_SOUND_ANALOG_PIN = ${text_big_sound_analog_pin}; 
+	int BIG_SOUND_DIGITAL_PIN = ${text_big_sound_digital_pin}; 
+  	#END
+	#SETUP
+	  pinMode(BIG_SOUND_ANALOG_PIN, INPUT);
+	  pinMode(BIG_SOUND_DIGITAL_PIN, INPUT);
+	#END
+	    
+	 
+	  float ${variable_big_sound_analog_value} = analogRead (BIG_SOUND_ANALOG_PIN);
+	  int ${variable_big_sound_digital_value} = digitalRead (BIG_SOUND_DIGITAL_PIN);
+	  
+	  delay (200);
+	
+    `;
+    return code;
+  };
